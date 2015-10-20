e3af54d60bb648a6a571ef93eb58a45646ff95bd
Chad Hoang <chad.hoang@yahoo.com>
2015-10-20 15:45:47 -0500

    Build 12
    Added demo visuals
    Tested a bit with aspect ratio for mobile phones

--- MobilePuzzle/Assets/_Scripts/Controller.cs
+++ MobilePuzzle/Assets/_Scripts/Controller.cs
@@ -21,7 +21,7 @@ public class Controller : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonUp("Fire1"))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
